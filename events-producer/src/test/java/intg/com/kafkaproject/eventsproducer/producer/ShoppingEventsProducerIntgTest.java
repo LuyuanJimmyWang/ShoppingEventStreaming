f605cb1d10b582a662be4a7f69fbdcd2655d2b6b
unknown <wangluyuan01@gmail.com>
2024-01-02 10:52:58 +0800

    update tests

--- events-producer/src/test/java/intg/com/kafkaproject/eventsproducer/producer/ShoppingEventsProducerIntgTest.java
+++ events-producer/src/test/java/intg/com/kafkaproject/eventsproducer/producer/ShoppingEventsProducerIntgTest.java
@@ -1,12 +1,93 @@
 package com.kafkaproject.eventsproducer.producer;
 
+import com.kafkaproject.eventsproducer.TestUtil;
+import com.kafkaproject.eventsproducer.dto.ShoppingEvent;
 import org.junit.jupiter.api.Test;
+import org.springframework.kafka.support.SendResult;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafkaproject.eventsproducer.dto.ShoppingEvent;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = "shopping-events")
+@TestPropertySource(properties = {
+        "spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}",
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"
+})
 class ShoppingEventsProducerIntgTest {
 
+    @Autowired
+    private ShoppingEventsProducer shoppingEventsProducer;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${spring.kafka.topic}")
+    private String topic;
+
+    private Consumer<Integer, String> consumer;
+
+    @BeforeEach
+    void setUp() {
+        var configs = new HashMap<>(KafkaTestUtils.consumerProps("group1", "true", embeddedKafkaBroker));
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        consumer = new DefaultKafkaConsumerFactory<>(configs, new IntegerDeserializer(), new StringDeserializer()).createConsumer();
+        embeddedKafkaBroker.consumeFromAllEmbeddedTopics(consumer);
+    }
+
+    @AfterEach
+    void tearDown() {
+        consumer.close();
+    }
+
     @Test
-    void sendShoppingEvent() {
+    void postSendShoppingEvent() throws Exception {
+        // Given
+        ShoppingEvent shoppingEvent = TestUtil.newShoppingEventRecordWithShoppingEventId();
+
+        // When
+        SendResult<Integer, String> sendResult = shoppingEventsProducer.sendShoppingEvent(shoppingEvent).get();
+
+        // Then
+        assertEquals("shopping-events", sendResult.getRecordMetadata().topic());
+
+        // Assertion on consumer receivable
+        ConsumerRecords<Integer, String> consumerRecords = KafkaTestUtils.getRecords(consumer);
+        assertEquals(1, consumerRecords.count());
+
+        consumerRecords.forEach(record -> {
+            var shoppingEventActual = TestUtil.parseShoppingEventRecord(objectMapper, record.value());
+            System.out.println("actual event: " + shoppingEventActual);
+            assertEquals(shoppingEventActual, TestUtil.newShoppingEventRecordWithShoppingEventId());
+        });
     }
 }
--- events-producer/src/test/java/unit/com/kafkaproject/eventsproducer/producer/ShoppingEventsProducerUnitTest.java
+++ events-producer/src/test/java/unit/com/kafkaproject/eventsproducer/producer/ShoppingEventsProducerUnitTest.java
@@ -1,12 +1,94 @@
 package com.kafkaproject.eventsproducer.producer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kafkaproject.eventsproducer.dto.ShoppingEvent;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.kafka.support.TopicPartitionOffset;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.function.ToDoubleBiFunction;
+
+import static com.kafkaproject.eventsproducer.TestUtil.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(MockitoExtension.class)
 class ShoppingEventsProducerUnitTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @InjectMocks
+    ShoppingEventsProducer shoppingEventsProducer;
+
+    @Spy
+    ObjectMapper objectMapper;
+
+    @Mock
+    KafkaTemplate<Integer, String> kafkaTemplate;
 
     @Test
-    void sendShoppingEvent() {
+    void sendShoppingEvent_HappyPath() throws Exception {
+        // 1. mock function postLibraryEvent
+        ShoppingEvent shoppingEvent = newShoppingEventRecordWithShoppingEventId();
+
+        String record = objectMapper.writeValueAsString(shoppingEvent);
+
+        // CompletableFuture<SendResult<Integer, String>> completableFuture = new CompletableFuture<SendResult<Integer, String>>();
+        ProducerRecord<Integer, String> producerRecord = new ProducerRecord("shopping-events", shoppingEvent.eventId(), record);
+        TopicPartition topicPartition = new TopicPartition("shopping-events", 1);
+        RecordMetadata recordMetadata = new RecordMetadata(topicPartition,1, 1, 1, 1, 1);
+        SendResult<Integer, String> sendResult = new SendResult(producerRecord, recordMetadata);
+        var future = CompletableFuture.supplyAsync(() -> sendResult);
+        when(kafkaTemplate.send(null, shoppingEvent.eventId(), record)).thenReturn(future);
+        var completableFuture = shoppingEventsProducer.sendShoppingEvent(newShoppingEventRecordWithShoppingEventId());
+
+        SendResult<Integer, String> actualSendResult = completableFuture.get();
+        assert actualSendResult.getRecordMetadata().partition() == 1;
     }
+
+    @Test
+    void sendShoppingEvent_BadPath() throws Exception {
+        ShoppingEvent shoppingEvent = shoppingEventRecordUpdateWithNullShoppingEventId();
+
+        String record = objectMapper.writeValueAsString(shoppingEvent);
+
+        CompletableFuture<SendResult<Integer, String>> completableFuture = new CompletableFuture<>();
+        completableFuture.completeExceptionally(new RuntimeException("Error sending the message"));
+
+        when(kafkaTemplate.send(null, shoppingEvent.eventId(), record)).thenReturn(completableFuture);
+
+        var actualCompletableFuture = shoppingEventsProducer.sendShoppingEvent(shoppingEvent);
+
+        assertThrows(Exception.class, () -> actualCompletableFuture.get(), "Error sending the message");
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
